4cc52e38c77b2a24bf2c708a132a3eef91ecc6c9
raintrace <947136704@qq.com>
2019-11-09 18:47:23 -0800

    add a filed

--- lab2/bctwo.c
+++ lab2/bctwo.c
@@ -1,12 +1,20 @@
 #include  <stdio.h>
 int main( )
-{
-    int a ;
-a=getchar( );
-while (a !=EOF)
-{
-    putchar (a);
-   a=getchar( );
-   }   
-   printf("a=%d",a); 
+{  int count=0;
+int b=1;
+   char a;
+  unsigned  int c=0;
+
+   while((a=getchar( )!='\n')){
+   c=c*10+(a-'0');
+   }
+
+   while (c!=0){
+   b=b*2;
+    c=c&(b);
+    count++;
+}
+   
+printf("%d\n",count);
+return 0;
 }
